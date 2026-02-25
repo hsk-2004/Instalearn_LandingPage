072b50bce72ef83215c650873b036871802fb8b5
hsk-2004 <harman.singh.23cse@bmu.edu.in>
2026-02-25 13:44:19 +0530

    Upgrade all Hero section assets to SVG for maximum quality

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
             <div className="mb-8">
                 <div className="w-56 h-12 relative flex items-center justify-center">
                     <Image
-                        src="/mobilefirst.png"
+                        src="/mobilefirst.svg"
                         alt="Mobile First Learning"
                         fill
                         className="object-contain"
@@ -28,7 +28,7 @@ export default function Hero() {
             {/* Main Image - No Box Container */}
             <div className="relative w-full max-w-[360px] mx-auto mt-2">
                 <img
-                    src="/mainimage.png"
+                    src="/mainimage.svg"
                     alt="Main Learning Interface"
                     className="w-full h-auto object-contain"
                 />
